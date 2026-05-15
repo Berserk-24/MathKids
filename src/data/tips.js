bd737b37150e007b6513370041933af0427d102a
DanielLaRotta <166852222+DanielLaRotta@users.noreply.github.com>
2026-05-14 23:01:09 -0500

    Revert "Tips y Randomizacion de fracciones"
    This reverts commit d50c55ea74d549a10944020d7df7f5c2ecd82b9b.

--- src/data/tips.js
+++ src/data/tips.js
@@ -5,24 +5,24 @@
 
 export const TIPS = {
   sumas: {
-    videoId: '6hd-gQod2QI',
-    title:   'Descompón los números en partes más pequeñas para sumar de forma más fácil y entender mejor cada resultado. Aqui un video que te puede ecplicar esto de una manera sencilla.',
+    videoId: '4y-4Tctgq6Y',
+    title:   'Cómo sumar fácil',
   },
   restas: {
-    videoId: 'QkDl2jdoTXM',
-    title:   'Aprende a restar paso a paso de una manera más clara y sencilla. Este método te ayudará a resolver operaciones con mayor facilidad y menos errores.',
+    videoId: '4y-4Tctgq6Y',
+    title:   'Cómo restar fácil',
   },
   multiplicacion: {
-    videoId: 'MqDL4uLbFI8',
-    title:   'Cuando multipliques números grandes usa el método de multiplicación por líneas, pero recuerda contar con cuidado los puntos donde se cruzan las líneas para obtener el resultado correcto.',
+    videoId: '4y-4Tctgq6Y',
+    title:   'Las tablas de multiplicar',
   },
     division: {
-    videoId: 'yFjmoaakbxU',
-    title:   'Para las divisiones te recomiendo el método ABN. Este metodo divide usando cantidades que conozcas fácilmente para avanzar paso a paso y entender mejor la operación. Aqui un video que te lo explica de manera sencilla.',
+    videoId: '4y-4Tctgq6Y',
+    title:   'Cómo dividir fácil',
   },
   fracciones: {
-    videoId: 'FUbla-rPt3M',
-    title:   'Antes de sumar fracciones, asegúrate de que tengan el mismo denominador para que la operación sea mucho más sencilla. Cuando restes fracciones, trabaja primero con denominadores iguales para evitar confundirte en el resultado. En la multiplicación de fracciones, multiplica numerador con numerador y denominador con denominador sin cambiar los términos. Para dividir fracciones, invierte la segunda fracción y luego multiplica normalmente paso a paso.',
+    videoId: '4y-4Tctgq6Y',
+    title:   'Qué son las fracciones',
   },
 }
 
